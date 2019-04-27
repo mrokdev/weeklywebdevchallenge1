f6b04cfb44f579b5275b7c80de3a3cc22a1d7604
mrokdev <mroku81@gmail.com>
2019-04-27 23:49:24 +0200

    Small changes in HTML and  action on placeholder.

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+window.onload = load;
 console.log('skrypt załadowany');
 
-const text = document.querySelector('#User_name');
+function load(){
+    console.log('strona zaladowana');
+    
+    var placeHold = document.querySelector('input[name=user_name]');
+    
 
-console.dir(text);
+    let txt = placeHold.placeholder;
+
+    console.log(placeHold.placeholder);
+    console.log(placeHold.type);
+    console.log(document.forms.length);
+   
+
+    placeHold.addEventListener('focus', firstEvent, false);
+    placeHold.addEventListener('blur', secondEvent, false);
+    
+    function firstEvent(){
+        placeHold.placeholder=' ';
+    }
+    function secondEvent(){  
+        placeHold.placeholder=txt;
+    }
+}
